e292cbaf307840b028993a7cd9f1bf0a1f5f3a1d
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2023-10-07 18:08:56 +0100

    created question with score

--- src/Question.js
+++ src/Question.js
@@ -1,11 +1,11 @@
 import Option from "./components/Option";
 
-const Question = ({ question }) => {
+const Question = ({ question, dispatch, answer }) => {
   console.log(question);
   return (
     <div>
       <h4>{question.question}</h4>
-      <Option question={question} />
+      <Option question={question} dispatch={dispatch} answer={answer} />
     </div>
   );
 };
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,8 +1,21 @@
-const Option = ({ question }) => {
+const Option = ({ question, dispatch, answer }) => {
+  const alreadyAnswered = answer !== null;
+
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button className="btn btn-option" key={option}>
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            alreadyAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          disabled={alreadyAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {option}
         </button>
       ))}
